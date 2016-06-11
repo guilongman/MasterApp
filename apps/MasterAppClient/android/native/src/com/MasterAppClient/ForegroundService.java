38cd0337845985262ccf482f8c5a7c4b98f47918
Victor Bôa Juliani <victorjuliani@Victors-MacBook-Pro.local>
2016-06-11 13:36:02 -0300

    Add ios/android environment

---
+++ apps/MasterAppClient/android/native/src/com/MasterAppClient/ForegroundService.java
@@ -0,0 +1,5 @@
+package com.MasterAppClient;
+
+public class ForegroundService extends com.worklight.androidgap.WLForegroundService{
+	//Nothing to do here...
+}
---
+++ apps/MasterAppClient/iphone/native/www/default/worklight/checksum.js
@@ -0,0 +1,2 @@
+var WL_CHECKSUM = {"date":1465662863339,"machine":"172.22.41.193","checksum":4201166224};
+/* Date: Sat Jun 11 13:34:23 BRT 2016 */
