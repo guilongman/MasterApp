735cb65f0e40df04fb53f08e95a43def667cf595
Vitor Mesquita <vleal@br.ibm.com>
2016-06-12 07:46:11 -0300

    fixing cat page

--- apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
+++ apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1465725479112,"machine":"172.22.41.36","checksum":2575152864};
-/* Date: Sun Jun 12 06:57:59 BRT 2016 */
+var WL_CHECKSUM = {"date":1465727767256,"machine":"172.22.41.36","checksum":2368583470};
+/* Date: Sun Jun 12 07:36:07 BRT 2016 */
