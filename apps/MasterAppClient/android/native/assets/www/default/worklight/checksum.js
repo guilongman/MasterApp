19e20379abb8abde5752a7b134adbea2b9aa26e2
Vitor Mesquita <vleal@br.ibm.com>
2016-06-12 02:02:38 -0300

    change navbar color

--- apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
+++ apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1465705463252,"machine":"172.22.41.36","checksum":404341903};
-/* Date: Sun Jun 12 01:24:23 BRT 2016 */
+var WL_CHECKSUM = {"date":1465706670206,"machine":"172.22.41.36","checksum":2408623391};
+/* Date: Sun Jun 12 01:44:30 BRT 2016 */
