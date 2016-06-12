d176d8b817c6d73c0d3378799f9dc7a4f3c78e91
Vitor Mesquita <vleal@br.ibm.com>
2016-06-11 22:44:20 -0300

    fix chat location

--- apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
+++ apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1465694315161,"machine":"172.22.41.36","checksum":1503980601};
-/* Date: Sat Jun 11 22:18:35 BRT 2016 */
+var WL_CHECKSUM = {"date":1465695398827,"machine":"172.22.41.36","checksum":3658381258};
+/* Date: Sat Jun 11 22:36:38 BRT 2016 */
