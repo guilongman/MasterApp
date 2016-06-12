0d204b10524041f8c7721076bcc410094488a098
Vitor Mesquita <vleal@br.ibm.com>
2016-06-12 06:59:12 -0300

    add folder to modal

---
+++ apps/MasterAppClient/android/native/assets/www/default/js/login.js
@@ -0,0 +1,45 @@
+
+/* JavaScript content from js/login.js in folder common */
+		// Load the SDK Asynchronously
+      (function(d){
+         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         ref.parentNode.insertBefore(js, ref);
+       }(document));
+
+      // Init the SDK upon load
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1649050102086054', // App ID
+          channelUrl : '//'+window.location.hostname+'/channel', // Path to your Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+          
+        });
+
+        // listen for and handle auth.statusChange events
+        FB.Event.subscribe('auth.statusChange', function(response) {
+          if (response.authResponse) {
+        	  // get user top 100 like pages
+        	    FB.api('/me/likes?limit=100', function(response){
+        		//get user likes, and redirect JSON to Watson API (Web Service)
+        		console.log(response);
+        		//Redirect user to main screen (chat) 
+        		$("#main").load("pages/chat.html");
+        	}, {scope: 'user_likes'})
+        	
+          } 
+        });
+
+        // respond to clicks on the login and logout links
+        document.getElementById('auth-loginlink').addEventListener('click', function(){
+          FB.login();
+        });
+        
+        document.getElementById('auth-logoutlink').addEventListener('click', function(){
+          FB.logout();
+        }); 
+      } 
--- apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
+++ apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1465716702742,"machine":"172.22.41.36","checksum":723376584};
-/* Date: Sun Jun 12 04:31:42 BRT 2016 */
+var WL_CHECKSUM = {"date":1465725479112,"machine":"172.22.41.36","checksum":2575152864};
+/* Date: Sun Jun 12 06:57:59 BRT 2016 */
