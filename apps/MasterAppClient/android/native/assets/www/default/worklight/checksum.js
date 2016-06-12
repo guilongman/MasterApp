d1e5ef5ee4305b9532fafbaf75f055114295b4b8
Vitor Mesquita <vleal@br.ibm.com>
2016-06-12 07:49:50 -0300

    add status bar

--- apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
+++ apps/MasterAppClient/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"date":1465727767256,"machine":"172.22.41.36","checksum":2368583470};
-/* Date: Sun Jun 12 07:36:07 BRT 2016 */
+var WL_CHECKSUM = {"date":1465728528291,"machine":"172.22.41.36","checksum":3048355761};
+/* Date: Sun Jun 12 07:48:48 BRT 2016 */
